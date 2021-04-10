91e092e7822d9d178974c1c522a86ae2d392c4d8
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-10 18:47:08 +0300

    icon && fontawesome added

--- src/app/components/Sidebar/index.js
+++ src/app/components/Sidebar/index.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Image from 'react-bootstrap/Image'
 import './style.css'
 
 const Sidebar = () => {
   return (
     <div className="sidebar-container">
-      <Link to="/dashboard/about">About</Link>
+      <div className="login-user">
+        <i className="fas fa-user-circle osman"></i>
+        <div className="login-info">
+          <h3 className="links"> Osman Abi</h3>
+          <h6 className="links">osmanabi@gmail.com</h6>
+        </div>
+      </div>
+      <div className="nav-item">
+        <Link to="/dashboard/about">
+          <i className="fas fa-object-ungroup kolor"></i>
+          <span className="links">Kategoriler</span>
+        </Link>
+      </div>
+      <div className="nav-item">
+        <Link to="/dashboard/about">
+          <i className="fas fa-atlas kolor"></i>
+          <span className="links">Kütüphaneler</span>
+        </Link>
+      </div>
+      <div className="nav-item">
+        <Link to="/dashboard/about">
+          <i className="fas fa-book kolor"></i>
+          <span className="links">Kitaplar</span>
+        </Link>
+      </div>
+      <div className="nav-item">
+        <Link to="/dashboard/about">
+          <i className="fas fa-walking kolor"></i>
+          <span className="links">Üyeler</span>
+        </Link>
+      </div>
+      <div className="nav-item">
+        <Link to="/dashboard/about">
+          <i className="fas fa-feather kolor"></i>
+          <span className="links">Yazarlar</span>
+        </Link>
+      </div>
     </div>
   )
 }
