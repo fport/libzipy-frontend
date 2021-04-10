a8f9387ad9b2682ad1f4e086a76b06ac351c5f98
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-10 20:52:47 +0300

    landing page designed

--- src/app/screens/LandingPage.js
+++ src/app/screens/LandingPage.js
@@ -1,10 +1,28 @@
 import { Navbarr } from '../components'
+import { libraryLanding } from '../assets'
+import './LandingPage.css'
+import { Button } from 'react-bootstrap'
 
 const LandingPage = () => {
   return (
     <>
       <Navbarr />
-      <div>landing</div>
+      <div className="lp-container">
+        <div className="lp-content">
+          <div className="lp-right-content">
+            <h1>Libzipy'e Hoşgeldin</h1>
+            <br />
+            <h6>Kitaplar ödünç alabilir, yazarlarıyla birebir görüşme fırsatı yakalayabilirsin.</h6>
+            <br />
+            <Button variant="danger">
+              Hemen üye ol<i class="fab fa-angellist"></i>
+            </Button>
+          </div>
+          <div className="lp-left-content">
+            <img src={libraryLanding} className="lp-content-image" alt="content library" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- src/app/screens/LoginPage.js
+++ src/app/screens/LoginPage.js
@@ -1,4 +1,48 @@
+import { useState } from 'react'
+import './LoginScreen.css'
+
 const Login = () => {
-  return <div>login page</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <>
+      <div className="login-screen">
+        <form className="login-screen__form">
+          <h3 className="login-screen__title">Login</h3>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              className="input-box"
+              type="email"
+              required
+              id="email"
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              tabIndex={1}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input
+              className="input-box"
+              type="password"
+              required
+              id="password"
+              autoComplete="true"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              tabIndex={2}
+            />
+          </div>
+          <button type="submit" className="btn but">
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  )
 }
 export default Login
