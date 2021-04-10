2aded6f64e3c0e989c69076641704da4d791819a
Furkan Portakal <furkanportakalx@gmail.com>
2021-04-10 14:33:36 +0300

    bootstrap added

--- src/app/screens/LandingPage.js
+++ src/app/screens/LandingPage.js
@@ -1,3 +1,17 @@
-export default function LandingPage() {
-  return <div>LandingPage</div>
+import React from 'react'
+import { libraryLanding } from '../assets'
+
+const LandingPage = () => {
+  return (
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk of the card's
+        content.
+      </p>
+      <a className="btn btn-primary">Go somewhere</a>
+    </div>
+  )
 }
+
+export default LandingPage
