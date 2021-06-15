ad39d46dba7ecb39804b81c8b7e53fdc1a767d26
Furkan Portakal <furkanportakalx@gmail.com>
2021-06-15 20:03:18 +0300

    library edit process

--- src/app/infrastructure/views/Library/index.js
+++ src/app/infrastructure/views/Library/index.js
@@ -11,8 +11,14 @@ const Library = ({ history }) => {
   const library = useSelector((state) => state.ui.library.libraryListReducer)
   const { libraryList, loading } = library
 
+  const selectedData = useSelector((data) => data.domain.info.userInfo)
+
   const onClickHandle = (id) => {
     history.push(`library/${id}`)
+  }
+
+  const onClickOptionHandle = (id) => {
+    console.log('hello', id)
   }
 
   useEffect(() => {
@@ -31,9 +37,27 @@ const Library = ({ history }) => {
                 </div>
                 <LibraryInfo title={library.library_name} value={library.library_adress} />
                 <div className="library-container-item-left"></div>
-                <button className="btn-sm" onClick={() => onClickHandle(library.library_id)}>
-                  Detay
-                </button>
+                <div className="library-container-content-options">
+                  <button className="btn-sm" onClick={() => onClickHandle(library.library_id)}>
+                    Detay
+                  </button>
+                  {selectedData.user_isadmin == 1 ? (
+                    <>
+                      <button
+                        className="option center"
+                        onClick={() => onClickOptionHandle(library.library_id)}
+                      >
+                        <i className="fas fa-trash-alt" />
+                      </button>
+                      <button
+                        className="option center"
+                        onClick={() => onClickOptionHandle(library.library_id)}
+                      >
+                        <i className="fas fa-edit" />
+                      </button>
+                    </>
+                  ) : null}
+                </div>
               </div>
             ))
           : 'loading'}
